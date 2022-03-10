9c510ebe93221e6eec6f551f80be9ab5172c02c1
HimangshuCyber <73021388+HimangshuCyber@users.noreply.github.com>
2022-03-10 20:57:41 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function preload(){
+   //There is nothing here :( 
+}
+function setup(){
+    canvas = createCanvas(400, 300);
+    canvas.center();
+    background("#fff")
+}
+function clear_canvas(){
+    background("#fff")
+}
